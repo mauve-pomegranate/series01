b66ed69ab90af3065c100d0a75066dd0d1eb768e
mauve-pomegranate <jesterscourt@gmail.com>
2025-07-12 12:06:20 -0400

    Update data_overview.js
    removed "By book" portion

--- data_overview.js
+++ data_overview.js
@@ -5,7 +5,7 @@ const overviewData = {
             "content": "<p>Across fractured realities and hidden worlds, a diverse group of individuals—an American expatriate, a half-kitsune investigator, a hardened operative, and resilient survivors of a broken America—must unravel ancient prophecies, confront corporate conspiracies, and battle cosmic entities to prevent the annihilation of all existence, forcing them to question the nature of identity, sacrifice, and the threads that bind family and fate.</p>"
         },
         {
-            "title": "Working Titles (By Book)",
+            "title": "Working Titles",
             "content": "<ul><li>Book 1: Fate and Foxfire</li><li>Book 2: Family and Folklore</li><li>Book 3: Faith and Finality</li></ul>"
         },
         {
