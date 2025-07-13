c27aa2619476ddfb5f29424a9362159bf3b00d27
mauve-pomegranate <jesterscourt@gmail.com>
2025-07-13 01:48:30 -0400

    OVERHAUL A
    this is the first iteration (newly added 7/13/25)

---
+++ data_fauna.js
@@ -0,0 +1,19 @@
+const faunaData = {
+    "Fauna": {
+        isAccordion: false,
+        items: [
+            {
+                "title": "Cats (General)",
+                "content": "<p><strong>Ætheric Sensitivity:</strong> Naturally attuned to the <a href=\"#/search/The Æther\" class=\"internal-link\">Æther</a>, spirits, and supernatural phenomena.</p><br><p><strong>Symbolism:</strong> Intuition, mystery, independence, guardians of hidden knowledge.</p>"
+            },
+            {
+                "title": "Kitty Coalition",
+                "content": "<p><strong>Nature:</strong> Three seemingly ordinary tabby cats acting in highly synchronized, intelligent ways.</p><br><p><strong>Role:</strong> Agents of <a href=\"#/search/Mr. Whispers\" class=\"internal-link\">Mr. Whispers</a>, guiding <a href=\"#/search/Alex 'Bowie' Shepard\" class=\"internal-link\">Bowie</a> to clues (bookstall, bus stop trash can with Yume's ticket, shrine). Act in military-like formation, stand on hind legs, beckon with paws. Hiss at vagrant. Drink cat milk from bowl with \"パ\" symbol.</p>"
+            },
+            {
+                "title": "Irradiated Crabs & Crossbred Evasive Lemur Species",
+                "content": "<p>Mentioned as part of <a href=\"#/search/Ikanajima Island\" class=\"internal-link\">Ikanajima Island</a>'s sparse wildlife, hinting at past/ongoing environmental contamination or unusual biological experiments.</p>"
+            }
+        ]
+    }
+};
