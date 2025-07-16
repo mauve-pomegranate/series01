0a4ab51084747b4961d0d4094085f2ac4625a867
mauve-pomegranate <jesterscourt@gmail.com>
2025-07-16 16:47:54 -0400

    Create data_timeline.js

---
+++ data_timeline.js
@@ -0,0 +1,99 @@
+const timelineData = {
+    "Book 1: Fate and Foxfire": {
+        items: [
+            {
+                "title": "Yume's Disappearance",
+                "description": "Yume Minamoto disappears after a cryptic tarot reading with <a href=\"#/search/Ichiro Kobayashi\" class=\"internal-link\">Ichiro Kobayashi</a> in <a href=\"#/search/Watanabe Park (Tokyo)\" class=\"internal-link\">Watanabe Park</a>.",
+                "event_num": "1"
+            },
+            {
+                "title": "The Investigation Begins",
+                "description": "<a href=\"#/search/Alex 'Bowie' Shepard\" class=\"internal-link\">Bowie</a> is tasked with finding Yume. He and <a href=\"#/search/Mizuki / Hisoka\" class=\"internal-link\">Mizuki</a>, on her own investigation, form an uneasy alliance.",
+                "event_num": "2-3"
+            },
+            {
+                "title": "Corporate Espionage",
+                "description": "<a href=\"#/search/Shikako ('Onryō')\" class=\"internal-link\">Shikako</a> infiltrates Kagami Tenko R&D, uncovering illegal experiments.",
+                "event_num": "4"
+            },
+            {
+                "title": "The Ikanajima Incident",
+                "description": "The group converges on <a href=\"#/search/Ikanajima Island\" class=\"internal-link\">Ikanajima Island</a>. Bowie enters the <a href=\"#/search/Kozyrev Mirror\" class=\"internal-link\">Kozyrev Mirror</a>, fracturing reality and prompting the <a href=\"#/search/Celestial Council\" class=\"internal-link\">Celestial Council</a> to deploy a stasis bubble.",
+                "event_num": "5-7"
+            },
+            {
+                "title": "Hisoka Awakens",
+                "description": "Trapped in <a href=\"#/search/The Æther\" class=\"internal-link\">the Æther</a>, Mizuki undergoes trials and awakens her latent kitsune powers, embracing the name Hisoka.",
+                "event_num": "8"
+            },
+            {
+                "title": "Climax and Sacrifice",
+                "description": "<a href=\"#/search/Professor Shin'en Tajima\" class=\"internal-link\">Professor Tajima</a> sacrifices himself to disrupt Project Tsunagari. Yume merges with <a href=\"#/search/Yume Minamoto / Sakura\" class=\"internal-link\">Sakura's</a> essence.",
+                "event_num": "9-10"
+            },
+            {
+                "title": "Aftermath",
+                "description": "To protect him, Hisoka wipes Bowie's memory of the entire affair.",
+                "event_num": "11"
+            }
+        ]
+    },
+    "Book 2: Family and Folklore": {
+        items: [
+            {
+                "title": "Carly's Search",
+                "description": "In the <a href=\"#/search/New American Republic (NAR)\" class=\"internal-link\">NAR</a>, <a href=\"#/search/Carly Shepard\" class=\"internal-link\">Carly Shepard</a> begins her search for her missing brother, Alex.",
+                "event_num": "1"
+            },
+            {
+                "title": "A Digital Ghost",
+                "description": "Carly contacts data wrangler <a href=\"#/search/Aoi\" class=\"internal-link\">Aoi</a>, who guides her towards the hidden <a href=\"#/search/NAR Particle Accelerator\" class=\"internal-link\">Appalachian facility</a>.",
+                "event_num": "2-3"
+            },
+            {
+                "title": "An Unlikely Alliance",
+                "description": "Carly encounters local cryptids and forms an uneasy alliance with Zenko operative <a href=\"#/search/Shikako ('Onryō')\" class=\"internal-link\">Shikako</a> and the fugitive scientist <a href=\"#/search/Fredrik Sørensen\" class=\"internal-link\">Sørensen</a>.",
+                "event_num": "4-6"
+            },
+            {
+                "title": "A Brother Found",
+                "description": "Inside the facility, Carly discovers NAR-Bowie preserved in a <a href=\"#/search/Stasis Pods (NAR)\" class=\"internal-link\">stasis pod</a>.",
+                "event_num": "7-8"
+            },
+            {
+                "title": "Sacrifice and Revelation",
+                "description": "During a chaotic escape as the accelerator goes critical, NAR-Bowie sacrifices himself to save Carly. Aoi informs her of \"another\" Bowie in Japan.",
+                "event_num": "9-11"
+            }
+        ]
+    },
+    "Book 3: Faith and Finality": {
+        items: [
+            {
+                "title": "Journey to Japan",
+                "description": "<a href=\"#/search/Carly Shepard\" class=\"internal-link\">Carly</a> travels to Japan and meets the memory-wiped <a href=\"#/search/Alex 'Bowie' Shepard\" class=\"internal-link\">Japan-Bowie</a>.",
+                "event_num": "1-4"
+            },
+            {
+                "title": "A Fractured Self",
+                "description": "<a href=\"#/search/Yume Minamoto / Sakura\" class=\"internal-link\">Sakura</a> reveals Bowie's split nature to him, causing an existential crisis.",
+                "event_num": "5-6"
+            },
+            {
+                "title": "The Threat of the Void",
+                "description": "The <a href=\"#/search/Kitsune (Fox Spirits)\" class=\"internal-link\">Kitsune Council</a> discusses the new nihilistic threat of <a href=\"#/search/Koda\" class=\"internal-link\">Koda</a> and his goal of Sunyata.",
+                "event_num": "7"
+            },
+            {
+                "title": "Return to the Island",
+                "description": "The group returns to <a href=\"#/search/Ikanajima Island\" class=\"internal-link\">Ikanajima</a>, where Bowie re-merges with his other half's essence, becoming whole.",
+                "event_num": "8-9"
+            },
+            {
+                "title": "Final Confrontation",
+                "description": "Drawn by the re-merging, <a href=\"#/search/Koda\" class=\"internal-link\">Koda</a> appears and the final battle to save all realities begins and is won through their combined efforts.",
+                "event_num": "10-11"
+            }
+        ]
+    }
+};
